8f84ed02bfd07576464bdd9e6b1bb2cfc004b6fe
labaratoriya <design@labaratoriya.com>
2015-01-14 19:25:47 +0100

    Create first_B3_A.py

---
+++ first_B3_A.py
@@ -0,0 +1,15 @@
+# input two whole numbers, add them together and print the result to the screen
+
+def main():
+    print("Koji je tvoj omiljeni broj?")
+    yourNumber = int(raw_input())
+    a = yourNumber
+    print("Jedan nasumicni broj?")
+    yourBroj = int(raw_input())
+    b = yourBroj
+       
+    # results
+
+    sum = a*b
+    print("Kad se to sve pomnozi ispadne:")
+    print(sum)

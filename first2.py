500324b392da6efd352a7a16199ea371e5da775f
labaratoriya <design@labaratoriya.com>
2015-01-14 14:37:57 +0100

    Create first2.py

---
+++ first2.py
@@ -0,0 +1,10 @@
+# input two messages and output them to a user.
+
+def main():
+    print("message 1")
+    message1 = raw_input()
+    print("message 2")
+    message2 = raw_input()
+    
+    # results
+    print("Your first message was: {0} Your second message was: {1}".format(message1,message2))

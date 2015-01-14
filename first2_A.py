0082e8ecf3829e761173219bb002d9f158ec80ac
labaratoriya <design@labaratoriya.com>
2015-01-14 19:24:38 +0100

    Create first2_A.py

---
+++ first2_A.py
@@ -0,0 +1,14 @@
+def main():
+    print("Koji je tvoj jezik?")
+    yourLanguage = raw_input()
+    print("U kojem gradu zivis?")
+    yourCity = raw_input()
+    print("Koliko imas godina?")
+    yourAge = raw_input()
+    print("Kako se zove macak")
+    yourCat = raw_input()
+    
+
+    #results
+
+    print("Tvoj jezik je: {0}. Ti zivis u {1}. Imas godina {2}. Tvoj macak se zove {3}".format(yourLanguage,yourCity,yourAge,yourCat))

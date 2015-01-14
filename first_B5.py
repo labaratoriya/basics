00f3b90c431f8a98c0c744efb44f862b841d8f4d
labaratoriya <design@labaratoriya.com>
2015-01-14 14:43:44 +0100

    Create first_B5.py

---
+++ first_B5.py
@@ -0,0 +1,8 @@
+def main():
+    print("Is it raining outside? (y/n) ")
+    #ask the user about weather
+    response = raw_input()
+    if response == "y":
+        print("Remember to take umbrella")
+    else:
+        print("Enjoy your suny weather, and take your sunglasses!")

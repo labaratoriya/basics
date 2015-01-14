2cc98cce8124570bb8ee75439bbd7c04cb92c307
labaratoriya <design@labaratoriya.com>
2015-01-14 14:39:43 +0100

    Create first_B1.py

---
+++ first_B1.py
@@ -0,0 +1,10 @@
+# Exercise 2 Basics
+# Input two whole numbers, add them together and print the result to the screen.
+
+def main():
+    number1 = int(raw_input("First number "))
+    number2 = int(raw_input("Second number "))
+    zbroj = number1 + number2
+
+    print "rezultat je:", zbroj
+    print zbroj

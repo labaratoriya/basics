0a2d521d6708381ebe501847ba6a1b0b44e026ff
Labaratoriya <design@labaratoriya.com>
2015-01-23 13:25:28 +0100

    Update arithmetic.py

--- arithmetic.py
+++ arithmetic.py
@@ -26,3 +26,13 @@ a = 10
 b = 5
 c = a//b 
 print "Line 7 - Value of c is ", c
+
+>>> 
+Line 1 - Value of c is  31
+Line 2 - Value of c is  11
+Line 3 - Value of c is  210
+Line 4 - Value of c is  2
+Line 5 - Value of c is  1
+Line 6 - Value of c is  8
+Line 7 - Value of c is  2
+>>> 

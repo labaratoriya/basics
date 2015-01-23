7d9bedbe1f9bdec1914e4e057a2a7b8dbd629bb2
Labaratoriya <design@labaratoriya.com>
2015-01-23 13:30:44 +0100

    Create assignment.py

---
+++ assignment.py
@@ -0,0 +1,34 @@
+a = 21
+b = 10
+c = 0
+
+c = a + b
+print "Line 1 - Value of c is ", c
+
+c += a
+print "Line 2 - Value of c is ", c 
+
+c *= a
+print "Line 3 - Value of c is ", c 
+
+c /= a 
+print "Line 4 - Value of c is ", c 
+
+c  = 2
+c %= a
+print "Line 5 - Value of c is ", c
+
+c **= a
+print "Line 6 - Value of c is ", c
+
+c //= a
+print "Line 7 - Value of c is ", c
+>>>
+Line 1 - Value of c is  31
+Line 2 - Value of c is  52
+Line 3 - Value of c is  1092
+Line 4 - Value of c is  52
+Line 5 - Value of c is  2
+Line 6 - Value of c is  2097152
+Line 7 - Value of c is  99864
+>>>>
